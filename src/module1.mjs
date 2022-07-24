747957543ac613de5c70cfb2864d5a3d25762663
vipul jain <vipuljain8441@gmail.com>
2022-07-24 11:05:56 +0530

    Updates

--- src/module1.mjs
+++
@@ -1,7 +0,0 @@
-const a = "ganesh";
-const b = "rahul"
- const c= "harsh";
-const d = "pankaj";
-export default a;
-export {b};
-export {c};
--- src/module2.mjs
+++
@@ -1,5 +0,0 @@
-import { c,b } from "./module1.mjs";
-import ui  from './module1.mjs';
-console.log(ui);
-console.log(c);
-console.log(b);
